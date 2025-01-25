ceb47da24ec3d8f038722865117ffc3f365b47f7
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-01-25 13:11:54 -0300

    added more styles to the search bar error popup

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -4,47 +4,15 @@ import { useApiData } from "./../hooks/api_data_provider.jsx";
 import { useError } from "./../hooks/error_provider.jsx";
 
 const SearchBar = () => {
-  const searchBarRef = useRef();
+  // Context hooks
   const { fetchApiData, fetchApiDataDummy, getApiData } = useApiData();
   const { setNewError } = useError();
 
-  /**
-   * Handles the search event when the form is submitted
-   * @param {Event} event - The submit event
-   * @returns
-   */
-  const handleSearch = async (event) => {
-    event.preventDefault();
-    const inputValue = searchBarRef.current.value;
+  // Refs
+  const searchBarRef = useRef();
+  const popupContainerRef = useRef();
 
-    if (inputValue === "") {
-      setNewError("Input field is required");
-      return;
-    }
-
-    // Sanitize input
-    let inputData = null;
-    if (isIP(inputValue)) {
-      inputData = { ipAddress: inputValue };
-    } else if (isDomain(inputValue)) {
-      inputData = { domain: inputValue };
-    } else if (isEmail(inputValue)) {
-      inputData = { email: inputValue };
-    } else {
-      setNewError("Invalid input, should be IP, domain or email");
-      return;
-    }
-    try {
-      const data = await fetchApiData(inputData);
-    } catch (err) {
-      // TODO: also display any input error on a CSS popup in the search bar in the search bar
-    }
-    console.log(process.env.NODE_ENV, data); // TODO: Remove log, only for testing
-
-    // Clear input
-    searchBarRef.current.value = "";
-  };
-
+  // Icons
   const iconArrow = (
     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
       <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
@@ -60,17 +28,29 @@ const SearchBar = () => {
         required
         ref={searchBarRef}
       ></input>
-      <button type="submit" className="search-bar--btn" onClick={handleSearch}>
+      <button
+        type="submit"
+        className="search-bar--btn"
+        onClick={() => {
+          handleSearch(searchBarRef);
+        }}
+      >
         {iconArrow}
       </button>
-      <SearchBarPopup messageString={"Test message"}></SearchBarPopup>
+      <div className="search-bar--popup-container" ref={popupContainerRef}>
+        <SearchBarPopup
+          messageString={
+            "Custom error message that would be too large to fit on the screen if it were to be displayed on mobile to test out the styles"
+          }
+        ></SearchBarPopup>
+      </div>
     </div>
   );
 };
 
 const SearchBarPopup = ({ messageString }) => {
   return (
-    <div className="search-bar--popup-container">
+    <div className="search-bar--popup-wrapper">
       <div className="search-bar--popup">
         <p>{messageString}</p>
       </div>
@@ -79,8 +59,6 @@ const SearchBarPopup = ({ messageString }) => {
 };
 
 export default SearchBar;
-
-// Helper functions
 
 const isIP = (ip) => {
   const re = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
@@ -109,4 +87,41 @@ const isEmail = (email) => {
     return false;
   }
   return true;
+};
+
+/**
+ * Handles the search event when the form is submitted
+ * @param {Event} event - The submit event
+ * @returns
+ */
+const handleSearch = async (event, searchBarRef) => {
+  event.preventDefault();
+  const inputValue = searchBarRef.current.value;
+
+  if (inputValue === "") {
+    setNewError("Input field is required");
+    return;
+  }
+
+  // Sanitize input
+  let inputData = null;
+  if (isIP(inputValue)) {
+    inputData = { ipAddress: inputValue };
+  } else if (isDomain(inputValue)) {
+    inputData = { domain: inputValue };
+  } else if (isEmail(inputValue)) {
+    inputData = { email: inputValue };
+  } else {
+    setNewError("Invalid input, should be IP, domain or email");
+    return;
+  }
+  try {
+    const data = await fetchApiData(inputData);
+  } catch (err) {
+    // TODO: also display any input error on a CSS popup in the search bar in the search bar
+  }
+  console.log(process.env.NODE_ENV, data); // TODO: Remove log, only for testing
+
+  // Clear input
+  searchBarRef.current.value = "";
 };
